4b6b925c51f71436c0d48d0bc2d863c196e1270e
Max <raranmariano@hotmail.com>
2018-04-13 17:15:50 -0300

    comments and number keyboard

--- app/src/main/java/com/max/tgr/feztranslate/fragments/AlphabetFragment.kt
+++ app/src/main/java/com/max/tgr/feztranslate/fragments/AlphabetFragment.kt
@@ -24,6 +24,11 @@ import kotlinx.android.synthetic.main.fragment_alphabet.view.alphabet_keyboard_m
 
 class AlphabetFragment : Fragment() {
 
+    /*
+    these are the variables for printing in the right places on the screen
+    onMain is the translated part and the Symbols is the symbols that the person wrote
+    on the keyboard
+    */
     var printOnMain = ""
     var printSymbols = ""
 
@@ -31,18 +36,86 @@ class AlphabetFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val layout = inflater.inflate(R.layout.fragment_alphabet, container, false)
 
+        //setupButtons will set the right font and click listeners
+        //passing the layout because of the fragment wiew
         setupButtons(layout)
 
         return layout
     }
 
+    private fun setupButtons(out: View) {
+        //loading the custom font
+        val fezFont = Typeface.createFromAsset(context.assets, "fonts/Fez.ttf")
+
+        //getting the size of each keyboard row (how many button in each of them)
+        val row1Count = out.alphabet_keyboard_row1.childCount
+        val row2Count = out.alphabet_keyboard_row2.childCount
+        val row3Count = out.alphabet_keyboard_row3.childCount
+        val row4Count = out.alphabet_keyboard_row4.childCount
+        val totalButtons = row1Count + row2Count + row3Count + row4Count
+
+        //for loop for setting the font and the listener for each button
+        for (i in 0 until totalButtons) {
+            var view: View
+
+            Log.v("until", i.toString())
+
+            if (i < row1Count) {
+                view = out.alphabet_keyboard_row1.getChildAt(i)
+            } else if (i < row1Count + row2Count) {
+                view = out.alphabet_keyboard_row2.getChildAt(i - row1Count)
+            } else if (i < row1Count + row2Count + row3Count) {
+                view = out.alphabet_keyboard_row3.getChildAt(i - row1Count - row2Count)
+            } else {
+                view = out.alphabet_keyboard_row4.getChildAt(i - row1Count - row2Count - row3Count)
+            }
+
+            //checking if is a text view, and setting its font and click listener
+            if (view is TextView) {
+                view.typeface = fezFont
+                view.setOnClickListener { onClick(view.id) }
+            }
+        }
+
+        //setting the side textView for using the custom font
+        out.alphabet_symbol_view.typeface = fezFont
+        //setting the special buttons, clear and erase, their onClick listeners
+        out.alphabet_button_clear.setOnClickListener { onClick(out.alphabet_button_clear.id) }
+        out.alphabet_button_erase.setOnClickListener { onClick(out.alphabet_button_erase.id) }
+
+        //on click listener for hiding and showing the keyboard using the "hide" or "show" button
+        out.alphabet_button_hide.setOnClickListener {
+
+            if (out.alphabet_keyboard_main.height > 0) {
+                //for hiding the height is set to 0, and the button changes to "show"
+                out.alphabet_keyboard_main.layoutParams.height = 0
+                out.alphabet_button_hide.text = getString(R.string.button_show)
+            } else {
+                //for showing the button gets back on match parent and the button shows "hide"
+                out.alphabet_keyboard_main.layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
+                out.alphabet_button_hide.text = getString(R.string.button_hide)
+            }
+
+            //commit the changes
+            out.alphabet_keyboard_main.requestLayout()
+
+        }
+
+    }
+
     private fun onClick(id: Int) {
+        //gets the actual name of the id
         val idName = resources.getResourceEntryName(id)
 
+
         when (idName) {
+            //if is the erase button, clear the last character
+            //every part of this "when" will change the main textView AND the symbol textView
             "alphabet_button_erase" -> {
                 if (printOnMain.isNotEmpty()) {
 
+                    //as there are symbols that mean 2 letters and they are print as (xy) we need to
+                    //erase 4 characters instead of 1 sometimes
                     if (printOnMain.substring(printOnMain.length - 1, printOnMain.length) == ")") {
                         printOnMain = printOnMain.substring(0, printOnMain.length - 4)
                     } else {
@@ -54,10 +127,12 @@ class AlphabetFragment : Fragment() {
                     printSymbols = printSymbols.substring(0, printSymbols.length - 1)
                 }
             }
+            //clear vars
             "alphabet_button_clear" -> {
                 printOnMain = ""
                 printSymbols = ""
             }
+            //this one adds two letters as some symbols does that
             "alphabet_button_kq", "alphabet_button_uv" -> {
                 printOnMain += "(" + idName.substring(idName.length - 2, idName.length) + ")"
                 printSymbols += idName.last()
@@ -74,66 +149,15 @@ class AlphabetFragment : Fragment() {
                 printOnMain += "."
                 printSymbols += "."
             }
+            //this one is for the majority of the buttons
             else -> {
                 printOnMain += idName.last()
                 printSymbols += idName.last()
             }
         }
+        //setting the changed variables to their views
         activity.alphabet_main_view.text = printOnMain
         activity.alphabet_symbol_view.text = printSymbols
-    }
-
-    private fun setupButtons(out: View) {
-        val fezFont = Typeface.createFromAsset(context.assets, "fonts/Fez.ttf")
-
-        val row1Count = out.alphabet_keyboard_row1.childCount
-        val row2Count = out.alphabet_keyboard_row2.childCount
-        val row3Count = out.alphabet_keyboard_row3.childCount
-        val row4Count = out.alphabet_keyboard_row4.childCount
-        val totalButtons = row1Count + row2Count + row3Count + row4Count
-
-        Log.e("buttons", totalButtons.toString() + ", 1 " + row1Count.toString() + ", 2 " + row2Count.toString()
-                + ", 3 " + row3Count.toString() + ", 4 " + row4Count.toString())
-
-        for (i in 0 until totalButtons) {
-            var view: View
-
-            Log.v("until", i.toString())
-
-            if (i < row1Count) {
-                view = out.alphabet_keyboard_row1.getChildAt(i)
-            } else if (i < row1Count + row2Count) {
-                view = out.alphabet_keyboard_row2.getChildAt(i - row1Count)
-            } else if (i < row1Count + row2Count + row3Count) {
-                view = out.alphabet_keyboard_row3.getChildAt(i - row1Count - row2Count)
-            } else {
-                view = out.alphabet_keyboard_row4.getChildAt(i - row1Count - row2Count - row3Count)
-            }
-
-            if (view is TextView) {
-                view.typeface = fezFont
-                view.setOnClickListener { onClick(view.id) }
-            }
-        }
-
-        out.alphabet_symbol_view.typeface = fezFont
-        out.alphabet_button_clear.setOnClickListener { onClick(out.alphabet_button_clear.id) }
-        out.alphabet_button_erase.setOnClickListener { onClick(out.alphabet_button_erase.id) }
-
-        out.alphabet_button_hide.setOnClickListener {
-
-            if (out.alphabet_keyboard_main.height > 0) {
-                out.alphabet_keyboard_main.layoutParams.height = 0
-                out.alphabet_button_hide.text = getString(R.string.button_show)
-            } else {
-                out.alphabet_keyboard_main.layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
-                out.alphabet_button_hide.text = getString(R.string.button_hide)
-            }
-
-            out.alphabet_keyboard_main.requestLayout()
-
-        }
-
     }
 
 }
--- app/src/main/java/com/max/tgr/feztranslate/fragments/SymbolsFragment.kt
+++ app/src/main/java/com/max/tgr/feztranslate/fragments/SymbolsFragment.kt
@@ -4,17 +4,18 @@ import android.content.SharedPreferences
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.*
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.max.tgr.feztranslate.R
-import kotlinx.android.synthetic.main.fragment_symbols.symbol_main_view
-import kotlinx.android.synthetic.main.fragment_symbols.symbol_command_view
-import kotlinx.android.synthetic.main.fragment_symbols.view.symbol_keyboard_row1
-import kotlinx.android.synthetic.main.fragment_symbols.view.symbol_keyboard_row2
-import kotlinx.android.synthetic.main.fragment_symbols.view.symbol_command_view
+import kotlinx.android.synthetic.main.fragment_alphabet.view.*
+import kotlinx.android.synthetic.main.fragment_symbols.*
+import kotlinx.android.synthetic.main.fragment_symbols.view.*
 
 class SymbolsFragment : Fragment() {
 
+    //todo:change these variables
     var textToPrintK:String = ""
     var textToPrintP:String = ""
 
@@ -41,10 +42,76 @@ class SymbolsFragment : Fragment() {
 
     }
 
-    private fun onClick(id: Int){
+    private fun switchOnClick(text: String){
+
+        val key = getString(R.string.symbols_toKey)
+        val num = getString(R.string.symbols_toNumber)
+
+        Log.e("switch",text)
+
+        if(text == key){
+            symbol_keyboard_rows.layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
+            symbol_number_rows.layoutParams.height = 0
+            symbol_button_switch.text = num
+        }else{
+            symbol_keyboard_rows.layoutParams.height = 0
+            symbol_number_rows.layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
+            symbol_button_switch.text = key
+        }
+
+        symbol_number_rows.requestLayout()
+        symbol_keyboard_rows.requestLayout()
+
+    }
+
+    private fun numsOnClick(id: Int){
+
+        val idName = resources.getResourceEntryName(id)
+
+        //todo: derotate the nums
+        when (idName.last()) {
+            'e' ->{
+                if(textToPrintK.isNotEmpty() && textToPrintP.isNotEmpty()) {
+                    textToPrintK = textToPrintK.substring(0, textToPrintK.length - 1)
+                    textToPrintP = textToPrintP.substring(0, textToPrintP.length - 1)
+                }
+            }
+            else ->{
+                textToPrintK += idName.last()
+                textToPrintP += idName.last()
+            }
+
+        }
+
+        activity.symbol_main_view.text = textToPrintK
+        activity.symbol_command_view.text = textToPrintP
+
+    }
+
+    //fixme:fix this on click, not working important
+    private fun eraseOnClick(){
+
+        if(textToPrintK.isNotEmpty() && textToPrintP.isNotEmpty()) {
+            when (textToPrintP.substring(textToPrintP.length - 1, textToPrintP.length)) {
+                "A" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (up.length + 2))
+                "B" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (right.length + 2))
+                "C" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (down.length + 2))
+                "D" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (left.length + 2))
+                "E" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (rotateLeft.length + 2))
+                "F" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (rotateRight.length + 2))
+                "G" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (jump.length + 2))
+                else -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - 1)
+            }
+            textToPrintP = textToPrintP.substring(0, textToPrintP.length - 1)
+        }
+
+    }
+
+    private fun keysOnClick(id: Int){
+
+        val idName = resources.getResourceEntryName(id)
 
         getController()
-        val idName = resources.getResourceEntryName(id)
 
         if(idName.last() != 'e') {
             when (idName.last()) {
@@ -60,7 +127,8 @@ class SymbolsFragment : Fragment() {
             textToPrintK += ", "
             textToPrintP += idName.last()
         }else{
-            if(textToPrintP.isNotEmpty()) {
+            if(textToPrintK.isNotEmpty() && textToPrintP.isNotEmpty()) {
+                Log.e("erase", textToPrintP.substring(textToPrintP.length - 1, textToPrintP.length))
                 when (textToPrintP.substring(textToPrintP.length - 1, textToPrintP.length)) {
                     "A" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (up.length + 2))
                     "B" -> textToPrintK = textToPrintK.substring(0, textToPrintK.length - (right.length + 2))
@@ -79,23 +147,66 @@ class SymbolsFragment : Fragment() {
     }
 
     private fun setupButtons(out:View){
+        //custom font
         val fezFont = Typeface.createFromAsset(context.assets, "fonts/Fez.ttf")
 
-        for (i in 0 until out.symbol_keyboard_row1.childCount) {
-            val view = out.symbol_keyboard_row1.getChildAt(i)
+        //switch button on click listener
+        out.symbol_button_switch.setOnClickListener{ switchOnClick(out.symbol_button_switch.text.toString()) }
+
+        //getting the size of each keyboard row (how many button in each of them)
+        val keyRow1Count = out.symbol_keyboard_row1.childCount
+        val keyRow2Count = out.symbol_keyboard_row2.childCount
+        val totalKeyButtons = keyRow1Count + keyRow2Count
+
+        //for loop for setting the font and the listener for each key button
+        for (i in 0 until totalKeyButtons - 1) {
+            var view: View
+
+            Log.v("until", i.toString())
+
+            if (i < keyRow1Count) {
+                view = out.symbol_keyboard_row1.getChildAt(i)
+            } else{
+                view = out.symbol_keyboard_row2.getChildAt(i - keyRow1Count)
+            }
+
+            //checking if is a text view, and setting its font and click listener
             if (view is TextView) {
                 view.typeface = fezFont
-                view.setOnClickListener { onClick(view.id) }
+                view.setOnClickListener { keysOnClick(view.id) }
             }
         }
-        for (i in 0 until out.symbol_keyboard_row2.childCount) {
-            val view = out.symbol_keyboard_row2.getChildAt(i)
+
+        //getting the size of each keyboard row again (how many button in each of them)
+        val numRow1Count = out.symbol_number_keyboard_row1.childCount
+        val numRow2Count = out.symbol_number_keyboard_row2.childCount
+        val numRow3Count = out.symbol_number_keyboard_row3.childCount
+        val totalNumButtons = numRow1Count + numRow2Count + numRow3Count
+
+        //for loop for setting the font and the listener for each number button
+        for (i in 0 until totalNumButtons - 1) {
+            var view: View
+
+            Log.v("until", i.toString())
+
+            if (i < numRow1Count) {
+                view = out.symbol_number_keyboard_row1.getChildAt(i)
+            } else if (i < numRow1Count + numRow2Count) {
+                view = out.symbol_number_keyboard_row2.getChildAt(i - numRow1Count)
+            } else{
+                view = out.symbol_number_keyboard_row3.getChildAt(i - numRow1Count - numRow2Count)
+            }
+
+            //checking if is a text view, and setting its font and click listener
             if (view is TextView) {
-                if(i != out.symbol_keyboard_row2.childCount - 1)
                 view.typeface = fezFont
-                view.setOnClickListener { onClick(view.id) }
+                view.setOnClickListener { numsOnClick(view.id) }
             }
         }
+
+        //fixme: this onclick is not working *important*
+        out.symbol_button_erase.setOnClickListener { eraseOnClick() }
+        out.symbol_number_button_erase.setOnClickListener { eraseOnClick() }
 
         out.symbol_command_view.typeface = fezFont
     }
